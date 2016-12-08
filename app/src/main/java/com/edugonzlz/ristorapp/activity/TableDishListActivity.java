8e2ec2772429622ebc2b39a3c8d36a0b55520056
Eduardo González <fonologico@gmail.com>
2016-12-08 12:35:09 +0100

    Mostramos flecha back en toolbar

--- app/src/main/java/com/edugonzlz/ristorapp/activity/TableDishListActivity.java
+++ app/src/main/java/com/edugonzlz/ristorapp/activity/TableDishListActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import com.edugonzlz.ristorapp.R;
 import com.edugonzlz.ristorapp.fragment.DishListFragment;
@@ -26,6 +27,7 @@ public class TableDishListActivity extends AppCompatActivity{
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         FragmentManager fm = getFragmentManager();
 
@@ -37,5 +39,16 @@ public class TableDishListActivity extends AppCompatActivity{
             }
         }
 
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        boolean superValue = super.onOptionsItemSelected(item);
+
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+            return true;
+        }
+        return superValue;
     }
 }
